8a78bd3f10c16dc6f585aa794e2aadf5785c1bd1
Ankar Kumar Saha <ankarkumarankar572@gmial.com>
2023-03-18 12:00:55 +0600

    submit with error 2(b) task

--- 2b_longest.asm
+++ 2b_longest.asm
@@ -1,12 +1,14 @@
 .MODEL SMALL
 .STACK 100H
 .DATA
-   CNT DB 20
+   CNT Dw 20
    N DW 20
    IND DW   ?
-   MSG DB 50
-   T DB ?
-   mx db ?
+   MSG DB 'anmkabrs$'
+   str db 50
+   T Dw ?
+   mx dw ?
+   ct dw ?
    
    ;NEWLINE DB 0AH,0DH,'$'
 .CODE
@@ -14,28 +16,28 @@
 MAIN PROC  
     MOV AX,@DATA
     MOV DS,AX
-    MOV SI,0 
-    
-    SCAN:
-        MOV AH,1H  
-        INT 21H
-        CMP AL,0Dh
-        JZ END_SCAN
-        MOV MSG[SI],AL
-        INC SI
-        JMP SCAN
-    
-    END_SCAN: 
-      MOV MSG[SI],'$'
-      CALL NEWLINE
+    MOV SI,8 
+    mov ct,0 
+    ;SCAN:
+;        MOV AH,1H  
+;        INT 21H
+;        CMP AL,0Dh
+;        JZ END_SCAN
+;        MOV MSG[SI],AL
+;        INC SI
+;        JMP SCAN
+;    
+;    END_SCAN: 
+;      MOV MSG[SI],'$'
+;      CALL NEWLINE
       MOV N,SI 
       MOV CX,SI
       MOV SI,0  
       INC N
       MOV MX,'0'
-    OUTLOOP:  
+    OUTLOOP:   
         CMP MSG[SI],'$'
-        JE EXIT
+        JE store2
         MOV IND,SI
         MOV DI,SI 
         
@@ -51,35 +53,38 @@ MAIN PROC
            MOV SI,DI
            JMP INLOOP
            SKIP:  
-             MOV DL,MX
-             CMP CNT,DL
+             MOV Dx,MX
+             CMP CNT,Dx
              JG COUNT 
              ;MOV CNT,0 
              MOV SI,IND
              INC SI
              JMP OUTLOOP
            COUNT: 
-            MOV DL,CNT
-            MOV mx,DL
+            MOV Dx,CNT
+            MOV mx,Dx
+            sub si,mx
+            ;dec si
+            mov t,si
             MOV SI,IND
             INC SI 
             JMP OUTLOOP
-    ;LOOP OUTLOOP  
+    ;LOOP OUTLOOP
+    store2:
+        mov si,0  
+        mov ax,t
+        mov mx,ax
+        ;sub mx,'0'
+    ;mov cx,mx
+    mov ah,02
+    print: 
     
-    EXIT:
-    CALL NEWLINE
-    MOV AH,2H 
-    MOV DL,MX
-    INT 21H
-    MOV AH,4CH
-    INT 21H
-    MAIN ENDP
-    NEWLINE PROC
-    MOV DL, 0AH ; carriage return
-    MOV AH, 02H
-    INT 21H
-    MOV DL, 0DH ; newline feed   
-    INT 21H
-    RET
- NEWLINE ENDP 
+       mov dx,mx
+       int 21h 
+       mov ah,4ch
+       int 21h
+       
+  
+    
+
 END MAIN
