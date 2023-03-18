d100c88fa2a1e3c11faf383557653c7fc3f9650d
Ankar Kumar Saha <ankarkumarankar572@gmial.com>
2023-03-18 12:30:47 +0600

    all task completed

--- 2b_longest.asm
+++ 2b_longest.asm
@@ -4,7 +4,7 @@
    CNT Dw 20
    N DW 20
    IND DW   ?
-   MSG DB 'anmkabrs$'
+   MSG DB 'abcdabcdefxyzbcdeabijklm$'
    str db 50
    T Dw ?
    mx dw ?
@@ -16,24 +16,8 @@
 MAIN PROC  
     MOV AX,@DATA
     MOV DS,AX
-    MOV SI,8 
-    mov ct,0 
-    ;SCAN:
-;        MOV AH,1H  
-;        INT 21H
-;        CMP AL,0Dh
-;        JZ END_SCAN
-;        MOV MSG[SI],AL
-;        INC SI
-;        JMP SCAN
-;    
-;    END_SCAN: 
-;      MOV MSG[SI],'$'
-;      CALL NEWLINE
-      MOV N,SI 
-      MOV CX,SI
+    
       MOV SI,0  
-      INC N
       MOV MX,'0'
     OUTLOOP:   
         CMP MSG[SI],'$'
@@ -63,24 +47,31 @@ MAIN PROC
            COUNT: 
             MOV Dx,CNT
             MOV mx,Dx
-            sub si,mx
-            ;dec si
-            mov t,si
-            MOV SI,IND
+            
+           
+            MOV SI,IND  
+             mov t,si
             INC SI 
             JMP OUTLOOP
     ;LOOP OUTLOOP
     store2:
-        mov si,0  
-        mov ax,t
-        mov mx,ax
-        ;sub mx,'0'
-    ;mov cx,mx
+        mov si,t  
+       ; add si,'0'
+        mov cx,0
     mov ah,02
     print: 
-    
-       mov dx,mx
-       int 21h 
+       cmp cx,mx
+       je exit   
+        
+       mov DX,MX
+       int 21h
+       inc si
+       inc cx
+       
+       jmp PRINT
+       
+       
+    EXIT:
        mov ah,4ch
        int 21h
        
